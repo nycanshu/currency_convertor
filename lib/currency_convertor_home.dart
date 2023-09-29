eb3a03631a4b69b1c159de13f6c3794cb48a109c
Himanshu <112685847+nycanshu@users.noreply.github.com>
2023-09-29 14:56:32 +0530

    first commit
    adding all the basic files to github

---
+++ lib/currency_convertor_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class currencyConvertorHomePage extends StatelessWidget {
+  const currencyConvertorHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello World on 2nd page"),
+      ),
+    );
+  }
+}
