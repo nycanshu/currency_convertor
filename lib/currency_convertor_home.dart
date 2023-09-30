269465c2b370351dbd038766e4b3ccd631b081f4
Himanshu <112685847+nycanshu@users.noreply.github.com>
2023-09-30 17:22:46 +0530

    homepage added
    Added currency convertor home page that has all features. To perform

--- lib/currency_convertor_home.dart
+++ lib/currency_convertor_home.dart
@@ -5,9 +5,52 @@ class currencyConvertorHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+        color: Colors.blueAccent,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.green,
       body: Center(
-        child: Text("Hello World on 2nd page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "0",
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Enter the Amount in USD",
+                  hintStyle: TextStyle(color: Colors.black54),
+                  prefixIcon: Icon(Icons.monetization_on),
+                  prefixIconColor: Colors.black54,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("Click Me"),
+            ),
+          ],
+        ),
       ),
     );
   }
